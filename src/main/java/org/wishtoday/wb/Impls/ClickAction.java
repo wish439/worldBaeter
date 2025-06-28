a39eab538ed4279d8d0547afa32db362d83416e5
wishtoday238 <wishtoday238@gmail.com>
2025-06-28 15:52:17 +0800

    可进行点击按钮的动作

---
+++ src/main/java/org/wishtoday/wb/Impls/ClickAction.java
@@ -0,0 +1,14 @@
+package org.wishtoday.wb.Impls;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.inventory.ItemStack;
+
+@FunctionalInterface
+public interface ClickAction {
+    void click(Player player
+            , ItemStack item
+            , ClickType clickType
+            , InventoryAction action);
+}
