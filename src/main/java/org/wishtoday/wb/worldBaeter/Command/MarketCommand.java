6744f13f8eaad927d6e477f08566b1dfe8277bec
wishtoday238 <wishtoday238@gmail.com>
2025-07-03 00:18:17 +0800

    交易GUI逻辑框架完善

--- src/main/java/org/wishtoday/wb/worldBaeter/Command/MarketCommand.java
+++ src/main/java/org/wishtoday/wb/worldBaeter/Command/MarketCommand.java
@@ -5,13 +5,17 @@ import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataType;
 import org.wishtoday.wb.worldBaeter.GUI.NavGUI;
+import org.wishtoday.wb.worldBaeter.GUI.TradeGUI;
 
 public class MarketCommand {
     public static void registerCommand(Commands commands) {
         commands.register(
                 Commands.literal("market")
                         .executes(MarketCommand::openGUI)
+                        .then(Commands.literal("confirm")
+                                .executes(MarketCommand::confirm))
                         .build()
         );
     }
@@ -20,6 +24,30 @@ public class MarketCommand {
         CommandSender sender = context.getSource().getSender();
         if (!(sender instanceof Player player)) return 0;
         new NavGUI().open(player);
+        return 1;
+    }
+
+    private static int confirm(CommandContext<CommandSourceStack> context) {
+        CommandSender sender = context.getSource().getSender();
+        if (!(sender instanceof Player player)) return 0;
+        if (!player.getPersistentDataContainer().has(TradeGUI.IS_NEED_CONFIRM, PersistentDataType.BOOLEAN)) {
+            player.sendMessage("没有待确认的交易");
+            return 0;
+        }
+        if (Boolean.FALSE.equals(player.getPersistentDataContainer().get(TradeGUI.IS_NEED_CONFIRM, PersistentDataType.BOOLEAN))) {
+            player.sendMessage("没有待确认的交易");
+            return 0;
+        }
+        TradeGUI tradeGUI = TradeGUI.tradeGUIs.get(player.getUniqueId());
+        if (tradeGUI == null) {
+            player.sendMessage("没有待确认的交易");
+            return 0;
+        }
+        //effect start
+        player.sendMessage("成功");
+        //effect end
+        tradeGUI.open(player);
+        TradeGUI.tradeGUIs.remove(player.getUniqueId());
         return 1;
     }
 }
